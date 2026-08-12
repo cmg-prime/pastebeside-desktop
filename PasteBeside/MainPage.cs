835e835ef54f29c13914e2009df83f2a7f089dfd
cmgoodquist <cmgoodquist@protonmail.com>
2026-08-11 20:51:06 -0500

    Functionality - add Uno solution scaffolding

---
+++ PasteBeside/MainPage.cs
@@ -0,0 +1,17 @@
+namespace PasteBeside;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this
+            .Background(Theme.Brushes.Background.Default)
+            .Content(new StackPanel()
+            .VerticalAlignment(VerticalAlignment.Center)
+            .HorizontalAlignment(HorizontalAlignment.Center)
+            .Children(
+                new TextBlock()
+                    .Text("Hello Uno Platform!")
+            ));
+    }
+}
