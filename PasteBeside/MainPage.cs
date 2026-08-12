0da64374127875ad12ef2b46d042a5c84495eb14
cmgoodquist <cmgoodquist@protonmail.com>
2026-08-11 23:06:26 -0500

    Refactor - organize dummy elements corresponding roughly to expected UI layout

--- PasteBeside/MainPage.cs
+++ PasteBeside/MainPage.cs
@@ -6,12 +6,31 @@ public sealed partial class MainPage : Page
     {
         this
             .Background(Theme.Brushes.Background.Default)
-            .Content(new StackPanel()
-            .VerticalAlignment(VerticalAlignment.Center)
-            .HorizontalAlignment(HorizontalAlignment.Center)
-            .Children(
-                new TextBlock()
-                    .Text("Hello Uno Platform!")
-            ));
+            .Content(new Grid()
+				.RowDefinitions("*, 4*")
+				.Children(
+					new Grid()
+						.ColumnDefinitions("*,*,*,*")
+						.Children(
+							BasicBorder(new TextBlock().Text("Block one!")).Grid(column: 0),
+							BasicBorder(new TextBlock().Text("Block two!")).Grid(column: 1),
+							BasicBorder(new TextBlock().Text("Block three!")).Grid(column: 2),
+							BasicBorder(new TextBlock().Text("Block four!")).Grid(column: 3)
+					).Grid(row: 0),
+					new Grid()
+						.ColumnDefinitions("4*, *")
+						.Children(
+							BasicBorder(new TextBlock().Text("Hello Uno Platform!")).Grid(column: 0),
+							BasicBorder(new TextBlock().Text("Goodbye Uno Platform!")).Grid(column: 1)
+					).Grid(row: 1)
+				)
+			);
     }
+
+	private static Border BasicBorder(UIElement child) => new Border()
+		.BorderBrush("Gray")
+		.BorderThickness(1)
+		.Margin(1)
+		.Padding(1)
+		.Child(child);
 }
