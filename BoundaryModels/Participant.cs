1732e75700e7dd71749f52eca5b358aade68af83
Moritz Goodquist <cmgoodquist@gmail.com>
2026-08-24 23:51:27 -0400

    Refactor - rename Participant constructor parameter userId -> id for clarity and simplicity

--- BoundaryModels/Participant.cs
+++ BoundaryModels/Participant.cs
@@ -2,16 +2,14 @@ namespace BoundaryModels;
 
 public class Participant
 {
-    //TODO: rename parameter to just 'id'
-    public Participant(string userId, string? displayName = null)
+    public Participant(string id, string? displayName = null)
     {
-        Id = userId;
-        DisplayName = displayName ?? userId;
+        Id = id;
+        DisplayName = displayName ?? id;
     }
 
     public string Id { get; }
     public string DisplayName { get; }
     public string? ConnectionId { get; set; }
-    //TOOD: not actually nullable
     public string? HandshakeId { get; set; }
 }
